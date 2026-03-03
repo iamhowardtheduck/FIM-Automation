2f04c7d79dc509527d1a225dafccac2df7daf61c
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-03-03 15:53:46 -0500

    Update setup-workshop.sh

--- Scripts/setup-workshop.sh
+++ Scripts/setup-workshop.sh
@@ -47,6 +47,7 @@ echo
 
 # Create index template for cmdb.api-results
 curl -X PUT "http://localhost:30920/_index_template/cmdb.api-results" -H "Content-Type: application/json" -u "elastic-rocks:splunk-sucks" -d @/root/FIM-Automation/Index-Templates/cmdb.api-results.json
+curl -X PUT "http://localhost:30920/_index_template/cmdb-updates" -H "Content-Type: application/json" -u "elastic-rocks:splunk-sucks" -d @/root/FIM-Automation/Index-Templates/cmdb-updates.json
 
 # Enable workflows
 curl -X POST "http://localhost:30002/api/kibana/settings" -H "Content-Type: application/json" -H "kbn-xsrf: true" -H "x-elastic-internal-origin: featureflag" -u "elastic-rocks:splunk-sucks"  -d '{
